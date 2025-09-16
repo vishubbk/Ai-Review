584f3510ffd762b570a76204e45d51aa0da608fc
vishubbk <vishubbkup@gmail.com>
2025-09-16 09:59:35 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 // 🔹 CORS Configuration
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://ai-reviewgpt.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
